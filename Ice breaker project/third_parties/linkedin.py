d9c785bb3fcb5da9cd584c44990f87f3fd5d8240
Samudrala Santhosh <samudrala.santhosh.19cse@bmu.edu.in>
2025-01-25 08:21:20 +0530

    Changed Ice breaker Project

--- Ice breaker project/third_parties/linkedin.py
+++ Ice breaker project/third_parties/linkedin.py
@@ -4,8 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+
+# Always define a function with type of inputs defined so that it is clear for you and for others who see it, also if you want to set default 
+# values like `mock`` in this function
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):  
     """ Scrape information from linkedin profiles,
     Manually scrape the information from the Linkedin profile.
     """
+    
     
