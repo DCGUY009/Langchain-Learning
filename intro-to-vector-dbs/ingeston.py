8d93c0d3fa1f38408b2e01ab33d4afa06f101492
Samudrala Santhosh <samudrala.santhosh.19cse@bmu.edu.in>
2025-02-13 08:24:32 +0530

    Added imports for vectorstore

--- intro-to-vector-dbs/ingeston.py
+++ intro-to-vector-dbs/ingeston.py
@@ -1,5 +1,12 @@
 import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
 
 PINECONE_VECTOR_INDEX = "medium-blogs-embeddings-index"
 
@@ -7,3 +14,7 @@ PINECONE_VECTOR_INDEX = "medium-blogs-embeddings-index"
 
 if __name__ == "__main__":
     print("Ingesting")
+
+
+
+
