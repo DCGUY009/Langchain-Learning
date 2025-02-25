1d4891bae8fb5e3134641f66a559b5a873add2eb
Samudrala Santhosh <samudrala.santhosh.19cse@bmu.edu.in>
2025-02-25 08:25:00 +0530

    Remove redundant langsmith lines

--- Ice breaker project/Agents/linkedin_lookup_agent.py
+++ Ice breaker project/Agents/linkedin_lookup_agent.py
@@ -14,8 +14,9 @@ from langchain.agents import (
 from langchain import hub  # To import premade prompts by the community
 from tools.tools import get_profile_url_tavily
 
-LANGSMITH_TRACING = True
-LANGSMITH_ENDPOINT = "https://api.smith.langchain.com"
+# LANGSMITH_TRACING = True
+# LANGSMITH_ENDPOINT = "https://api.smith.langchain.com"  # These 2 lines are not needed for tracking the traces in 
+# langsmith 
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY1")
 print(OPENAI_API_KEY)
