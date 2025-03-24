094e396dcf7469631ad2edd8fc3e888a32f0669f
Samudrala Santhosh <samudrala.santhosh.19cse@bmu.edu.in>
2025-03-24 15:56:30 +0530

    Ingested V3 documentation as RAG in pinecone

--- Documentation Helper/ingestion.py
+++ Documentation Helper/ingestion.py
@@ -1,22 +1,30 @@
 import os 
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import ReadtheDocsLoader
+from langchain_community.document_loaders import ReadTheDocsLoader 
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from pprint import pprint 
 
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY1")
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small", 
+    api_key=OPENAI_API_KEY
+    )
+PINECONE_INDEX = "langchain-doc-index"
 
 
 def ingest_docs():
-    loader = ReadtheDocsLoader("./Documentation Scraping v3/latest/")
+    loader = ReadTheDocsLoader("D:/Genai_projects/Langchain - Develop LLM powered applications - Udemy Course/Documentation Helper/Documentation Scraping v3/langchain-docs/api.python.langchain.com/en/latest")
 
     raw_documents = loader.load()
+
+    # for document in raw_documents:
+    #     pprint(f"{document}\n---------------------\n")
 
     print(f"Loaded {len(raw_documents)} documents")
     """
@@ -29,9 +37,33 @@ def ingest_docs():
     totally dependent on the use case you are solving.
     """
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
+    documents = text_splitter.split_documents(raw_documents)
+
+    for doc in documents:
+        new_url = doc.metadata["source"]
+        new_url = new_url.replace(
+            "D:\\Genai_projects\\Langchain - Develop LLM powered applications - Udemy Course\\Documentation Helper\\Documentation Scraping v3\\langchain-docs", 
+            "https:/")
+        # Convert backslashes to forward slashes
+        new_url = new_url.replace("\\", "/")
+        doc.metadata.update({"source": new_url})
+    
 
 
-    pass
+    for document in documents:
+        pprint(f"{document}\n---------------------\n")
+    
+    print(f"Going to add {len(documents)} to Pinecone")
+
+    # for doc in documents:
+    #     print(f"Raw metadata source: {repr(doc.metadata['source'])}")
+
+
+    PineconeVectorStore.from_documents(
+        documents, embeddings, index_name=PINECONE_INDEX
+    )
+
+    print("*** Loading to Vector Store done ***")
 
 
 if __name__=="__main__":
