0bdd5a4e8c1ca98a7046d875d0c5f5ea9bf441cd
Samudrala Santhosh <samudrala.santhosh.19cse@bmu.edu.in>
2025-03-26 13:15:04 +0530

    Completed streamlit and added memory

--- Documentation Helper/backend/core.py
+++ Documentation Helper/backend/core.py
@@ -5,6 +5,8 @@ from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from typing import List, Dict, Any
 
 INDEX_NAME = "langchain-doc-index"
 
@@ -13,7 +15,7 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY1")
 os.environ["LANGSMITH_PROJECT"] = "Documentation Helper"
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     embeddings = OpenAIEmbeddings(
         model="text-embedding-3-small",
         api_key=OPENAI_API_KEY
@@ -36,17 +38,35 @@ def run_llm(query: str):
         retrieval_qa_chat_prompt
     )
 
-    qa = create_retrieval_chain(
+    # Ok, for making sure that we are answering follow up questions better we need to rephrase the prompt and create a retriever for 
+    # this as well
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+
+    # Langchain created a history to take this new question 
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat,
         retriever=docsearch.as_retriever(),
+        prompt=rephrase_prompt
+    )
+
+    # qa = create_retrieval_chain(
+    #     retriever=docsearch.as_retriever(),
+    #     combine_docs_chain=stuff_documents_chain
+    # )  # So, here the combine_docs_chain is used to do post processing of the documents that are retrieved based on our needs
+    # # So, after the documents are retrieved, maybe we want to summarize them or maybe we want some filtering or maybe some 
+    # # keyword extraction to happen whatever we want to do for such things we are putting it in combine_docs_chain. So, this gives us 
+    # # the flexibility to plugin our own funcitonality for some post processing after retrieval. 
+    # # TO-DO: Play around with this to see how we can modify the documents.
+
+    # Replacing the retriever in qa with history aware retriever
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever,
         combine_docs_chain=stuff_documents_chain
-    )  # So, here the combine_docs_chain is used to do post processing of the documents that are retrieved based on our needs
-    # So, after the documents are retrieved, maybe we want to summarize them or maybe we want some filtering or maybe some 
-    # keyword extraction to happen whatever we want to do for such things we are putting it in combine_docs_chain. So, this gives us 
-    # the flexibility to plugin our own funcitonality for some post processing after retrieval. 
-    # TO-DO: Play around with this to see how we can modify the documents.
+    )
 
     result = qa.invoke(input={
-        "input": query
+        "input": query,
+        "chat_history": chat_history
     })
     new_result = {
         "query": result["input"],
--- Documentation Helper/main.py
+++ Documentation Helper/main.py
@@ -6,6 +6,16 @@ st.header("Langchain v3 Documentation Helper")
 
 prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
 
+if (
+    "chat_answers_history" not in st.session_state
+    and "user_prompt_history" not in st.session_state
+    and "chat_history" not in st.session_state
+):
+    st.session_state["user_prompt_history"] = []
+    st.session_state["chat_answers_history"] = []
+    st.session_state["chat_history"] = []
+
+
 def create_sources_string(source_urls: Set[str]) -> str:
     if not source_urls:
         return ""
@@ -24,10 +34,23 @@ def create_sources_string(source_urls: Set[str]) -> str:
 
 if prompt:
     with st.spinner("Generating Response"):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt,
+            chat_history=st.session_state["chat_history"]
+            )
         sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])  
         # we are converting to set to eliminate duplicate links
 
         formatted_response = f"{generated_response["result"]} \n\n {create_sources_string(sources)}"
 
-        st.markdown(formatted_response) 
+        # st.markdown(formatted_response) 
+
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(formatted_response)
+        st.session_state["chat_history"].append(("human", prompt))
+        st.session_state["chat_history"].append(("ai", generated_response["result"]))
+
+if st.session_state["chat_answers_history"]:
+    for user_query, generated_response in zip(st.session_state["user_prompt_history"], st.session_state["chat_answers_history"]):
+        st.chat_message("user").write(user_query)
+        st.chat_message("assistant").write(generated_response)
